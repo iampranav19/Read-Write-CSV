a41a66f49d0bae8ba833d0c292aae06345520f3d
iampranav19 <46451477+iampranav19@users.noreply.github.com>
2023-01-25 22:22:01 +0530

    Update ReadCsv.java
    row and column read in the csv

--- ReadCsv.java
+++ ReadCsv.java
@@ -28,4 +28,35 @@ public class ReadCsv {
 			// TODO: handle exception
 		}
 	}
+	public static void readDataRowWise() throws IOException
+	{
+		File file = new File("D:\\\\\\\\temp\\\\\\\\hello.csv"); 
+		List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8); 
+		for (String line : lines) { 
+		   String[] array = line.split(";"); 
+		   System.out.println(array[0]+" "+array[array.length-1]); 
+		}
+	}
+	public static String[] readColumnWise(int columnInCsv, String filePath, String delimeter)
+	{
+		String[] data ;
+		String currentLine;
+		ArrayList<String> colData=new ArrayList();
+		try {
+			
+			FileReader fr=new FileReader(filePath);
+			BufferedReader br=new BufferedReader(fr);
+			while((currentLine =br.readLine())!=null)
+			{
+				data=currentLine.split(delimeter);
+				colData.add(data[col]);
+				
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			return null;
+		}
+		return colData.toArray(new String[0]);
+	}
 }
